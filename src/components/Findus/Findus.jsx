a44001f4de7770b41d69e13f8cdc6874f28ff1dc
Fatehajahan2002 <fatehajahan2002@gmail.com>
2025-02-01 11:22:26 +0600

    done

--- src/components/Findus/Findus.jsx
+++ src/components/Findus/Findus.jsx
@@ -23,10 +23,10 @@ const Findus = () => {
                         <div className='w-[80%]'>
                             <iframe
                                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d233668.06397445584!2d90.254873766693!3d23.780753027627565!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b8b087026b81%3A0x8fa563bbdd5904c2!2sDhaka!5e0!3m2!1sen!2sbd!4v1731478729133!5m2!1sen!2sbd"
-                                class="w-full h-full border-0 rounded-lg"
-                                allowfullscreen=""
+                                className="w-full h-full border-0 rounded-lg"
+                                allowFullScreen=""
                                 loading="lazy"
-                                referrerpolicy="no-referrer-when-downgrade">
+                                referrerPolicy="no-referrer-when-downgrade">
                             </iframe>
                         </div>
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,25 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FaSearch } from "react-icons/fa";
-
 import logo from '../../assets/logo.png'
+import { CiMenuFries } from "react-icons/ci";
+
+
+
+
 
 const Navbar = () => {
+    const [dropdown, setDropdown] =  useState(false)
+    const drop  =  () => {
+        console.log('sdf');
+        setDropdown(!dropdown);
+    }
+
     return (
         <div className='bg-[#fff] mt-[11px] '>
             <div className="container">
-                <div className="flex justify-between items-center py-[16px]">
-                    <div className="w-1/2">
+                <div className="md:flex justify-between items-center py-[16px]">
+                    <div className="flex justify-between items-center md:w-1/2">
                         <img src={logo} alt="" />
+                        <CiMenuFries onClick={ drop } className=' text-[30px] md:hidden block cursor-pointer' />
                     </div>
 
-                    <div className="w-1/2 ">
-                        <ul className='flex justify-end gap-x-[30px]'>
+                    <div className="md:w-1/2 ">
+                        <ul className="md:flex hidden justify-end gap-x-[30px] transition duration-700 ">
                             <li> <a href="#" className='text-[16px] font-rale font-bold text-[#F95C19] '>Home</a></li>
                             <li> <a href="#" className='text-[16px] font-rale font-bold text-[#817382] hover:text-[#F95C19] transition duration-700 '>Our services</a></li>
                             <li> <a href="#" className='text-[16px] font-rale font-bold text-[#817382] hover:text-[#F95C19] transition duration-700 '>About Us</a></li>
                             <li> <a href="#" className='text-[16px] font-rale font-bold text-[#817382] hover:text-[#F95C19] transition duration-700 '>What’s new?</a></li>
 
-                            <div className="search flex items-center gap-4">
+                            <div className="search md:flex items-center gap-4">
                                 <FaSearch className='text-[#FFAF0F] bg-[#FFF1DC] text-[22px] rounded' />
                                 <div className="btn"><a href="#" className='font-oxi bg-[#FFE4D9] text-[#F95C19] font-bold py-[12px] px-[20px] rounded hover:bg-[#F95C19] hover:text-[#FFE4D9] transition duration-500'>Contact us</a></div>
                             </div>
